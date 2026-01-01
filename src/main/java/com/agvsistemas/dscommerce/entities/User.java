8858f78b388ff54c164d284eccb158a5dc5aed5a
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-01-01 12:12:17 -0300

    implements user with roles

--- src/main/java/com/agvsistemas/dscommerce/entities/User.java
+++ src/main/java/com/agvsistemas/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.agvsistemas.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +18,12 @@ public class User {
     private String password;
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {}
 
@@ -82,6 +86,19 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String authority) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(authority)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
